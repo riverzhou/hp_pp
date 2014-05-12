83770a99e8f5cad07a31337d9dc24a08b1533171
River Zhou <riverzhou2000@gmail.com>
2014-05-12 12:55:25 +0800

    初始化UDP模块文件

---
+++ udp_client.c
@@ -0,0 +1,6 @@
+
+
+#include "udp_client.h"
+
+
+
---
+++ udp_client.h
@@ -0,0 +1,14 @@
+
+#ifndef UDP_CLIENT_H_INCLUDED
+#define UDP_CLIENT_H_INCLUDED
+
+//#include "user.h"
+
+//---------------------------------------------
+//---------------------------------------------
+
+
+//---------------------------------------------
+
+#endif // UDP_CLIENT_H_INCLUDED
+
