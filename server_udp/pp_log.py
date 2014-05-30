d9ca74a0d2a9e1727455c94942dd0f369576279b
River Zhou <riverzhou2000@163.com>
2014-05-30 23:03:50 +0800

    对 udp server 加入彩色log

--- server_udp/pp_log.py
+++ server_udp/pp_log.py
@@ -1,62 +1,105 @@
 #!/usr/bin/env python3
 
-
-
 import logging
 
+#=========================================================================================
 
-def make_log(name = None, log = None,  level = 'debug' , console = True, fmt = True):
+class ColoredFormatter(logging.Formatter):
+        BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(30, 38)
+
+        COLORS = {
+                'DEBUG'   : GREEN,
+                'INFO'    : WHITE,
+                'WARNING' : BLUE,
+                'ERROR'   : RED,
+                'CRITICAL': YELLOW,
+                }
+
+        RESET_SEQ = "\033[0m"
+        COLOR_SEQ = "\033[1;%dm"
+        BOLD_SEQ  = "\033[1m"
+
+        def format(self, record):
+                levelname = record.levelname
+                if levelname in self.COLORS :
+                        record.levelname = self.COLOR_SEQ % self.COLORS[levelname] + levelname + self.RESET_SEQ
+                return logging.Formatter.format(self, record)
+
+class foobar_logger():
+        def debug(self, log):
+                pass
+        def info(self, log):
+                pass
+        def warning(self, log):
+                pass
+        def error(self, log):
+                pass
+        def critical(self, log):
+                pass
+
+#-----------------------------------------------------------------------------------------
+
+def make_log(name = None, log = None,  level = 'debug' , console = True, fmt = True, color = True):
+
         LEVELS = {
                 'debug'   : logging.DEBUG,
                 'info'    : logging.INFO,
                 'warning' : logging.WARNING,
                 'error'   : logging.ERROR,
-                'critical': logging.CRITICAL}
+                'critical': logging.CRITICAL
+                }
 
-        if not level in LEVELS :
-                level = 'debug' 
-        else:
-                level = LEVELS[level]
+        level = LEVELS[level] if level in LEVELS else 'debug'
 
         # 创建一个logger
-        if log != None :
-                logger = logging.getLogger(log)
-        else :
-                logger = logging.getLogger()
-
+        logger = logging.getLogger(log) if log != None else logging.getLogger()
         logger.setLevel(logging.DEBUG)
 
         # 定义handler的输出格式
-        if fmt != False :
-                #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-                formatter = logging.Formatter('%(asctime)s - %(levelname)-8s ->|%(message)s')
-        else :
-                formatter = logging.Formatter()
+        COLOR_FORMAT      = '%(asctime)s - %(levelname)-19s ->|%(message)s'
+        NORMAL_FORMAT     = '%(asctime)s - %(levelname)-8s ->|%(message)s'
+
+        color_formatter   = ColoredFormatter(COLOR_FORMAT)
+        normal_formatter  = logging.Formatter(NORMAL_FORMAT)
+        none_formatter    = logging.Formatter()
+
+        color_formatter   = color_formatter   if color == True else normal_formatter 
+        screen_formatter  = color_formatter   if fmt == True   else none_formatter
+        file_formatter    = normal_formatter  if fmt == True   else none_formatter
 
         # 创建一个handler，用于写入日志文件，再添加到logger
         if name != None :
                 fh = logging.FileHandler(name)
                 fh.setLevel(level)
-                fh.setFormatter(formatter)
+                fh.setFormatter(file_formatter)
                 logger.addHandler(fh)
 
         # 再创建一个handler，用于输出到控制台，添加到logger
         if console != False :
                 ch = logging.StreamHandler()
                 ch.setLevel(level)
-                ch.setFormatter(formatter)
+                ch.setFormatter(screen_formatter)
                 logger.addHandler(ch)
-
-        # 记录一条日志
-        #logger.info('foorbar')
 
         return logger
 
+#=========================================================================================
 
-logger     = make_log(name = 'log.log',     log = 'logger',     level = 'info',    console = False, fmt = True)
-printer    = make_log(name = 'pri.log',     log = 'printer',    level = 'info',    console = False, fmt = False)
+logger     = make_log(name = 'log.log',     log = 'logger',     level = 'debug', console = True, fmt = True,  color = True)
+printer    = make_log(name = 'pri.log',     log = 'printer',    level = 'debug', console = True, fmt = False, color = False)
 
-#ct_printer = make_log(name = 'ctpri.log' , log = 'ct_printer', level = 'info',    console = False, fmt = False)
-#pp_printer = make_log(name = 'pppri.log' , log = 'pp_printer', level = 'info',    console = False, fmt = False)
+#ct_printer = make_log(name = 'ctpri.log' , log = 'ct_printer', level = 'info',  console = False, fmt = False, color = True)
+#pp_printer = make_log(name = 'pppri.log' , log = 'pp_printer', level = 'info',  console = False, fmt = False, color = True)
 
+#logger     = foobar_logger()
+#printer    = foobar_logger()
+
+#================================ for test ===============================================
+
+if __name__ == "__main__":
+        logger.debug('test logger debug')
+        logger.info('test logger info')
+        logger.warning('test logger warning')
+        logger.error('test logger error')
+        logger.critical('test logger critical')
 
