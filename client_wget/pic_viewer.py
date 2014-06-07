86bb8b9fb9d55898df35230541a13f04ce047825
River Zhou <riverzhou2000@163.com>
2014-06-07 11:50:03 +0800

    wget 客户端 2.0 ，支持直接从image.ack中读取分析图片并在 GUI 中显示

---
+++ client_wget/pic_viewer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from traceback          import print_exc
+from base64             import b64decode
+from tkinter            import Tk, Label
+from PIL                import Image, ImageTk
+from io                 import BytesIO
+
+from pickle             import dump, load
+
+from pp_sslproto        import proto_ssl_image
+
+
+
+def get_image():
+        buff = open('image.ack','rb').read()
+        buff = buff.decode().encode('gb18030')
+        image = proto_ssl_image({}).parse_image_ack(buff)['image']
+        image = b64decode(image)
+        return image
+
+def show_photo(image):
+        root  = Tk()
+        root.title('Image')
+        label = Label()
+        photo = ImageTk.PhotoImage(Image.open(BytesIO(image)))
+        label.configure(image = photo)
+        label.pack()
+        root.mainloop()
+
+if __name__ == '__main__' :
+        show_photo(get_image())
+
