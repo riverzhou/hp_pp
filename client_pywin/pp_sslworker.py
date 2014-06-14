7bd3d76e67697240317fdf535cbc688520ac6cf0
riverzhou <riverzhou2000@163.com>
2014-06-14 15:32:22 +0800

    将pyget拆成2段，连接和通讯分离。为线程池做好了准备

--- client_pywin/pp_sslworker.py
+++ client_pywin/pp_sslworker.py
@@ -18,51 +18,67 @@ from pp_sslproto        import *
 #==========================================================
 
 class ssl_worker(pp_thread):
-        def __init__(self, info = ''):
+        def __init__(self, key_val, manager, info = ''):
                 pp_thread.__init__(self, info)
+                self.info       = info
+                self.manager    = manager
                 self.event_proc = Event()
                 self.arg        = None
+                self.handler    = None
+                self.host_ip    = key_val['host_ip']
+                self.host_name  = key_val['host_name']
+                self.timeout    = key_val['timeout'] if 'timeout' in key_val else None
+
+        def close(self):
+                if self.handler != None :
+                        try:
+                                self.handler.close()
+                        except:
+                                print_exc()
 
         def main(self):
-                self.event_proc.wait()
-                self.do_proc(self.arg)
-
-        def proc(self, arg):
-                self.arg = arg
-                self.event_proc.set()
-
-        def do_proc(self, arg): pass
-
-        def pyget(self, host, req, headers = {}, timeout = None):
                 while True:
-                        handler = HTTPSConnection(host, timeout = timeout)
-                        handler._http_vsn = 10
-                        handler._http_vsn_str = 'HTTP/1.0'
+                        self.handler = HTTPSConnection(self.host_ip, timeout = self.timeout)
+                        self.handler._http_vsn = 10
+                        self.handler._http_vsn_str = 'HTTP/1.0'
                         try:
-                                handler.connect()
+                                self.handler.connect()
                         except (sock_timeout, TimeoutError):
-                                handler.close()
+                                self.handler.close()
                                 continue
                         except:
                                 print_exc()
                                 continue
                         break
-                #--------------------------------------------------------
+                self.manager.feedback('connected', self)
+                self.event_proc.wait()
+                self.do_proc(self.arg)
+
+        def put(self, arg):
+                self.arg = arg
+                self.event_proc.set()
+
+        def do_proc(self, arg): pass
+
+        def pyget(self, req, headers = {}):
                 try:
-                        handler.request('GET', req, headers = headers)
+                        self.handler.request('GET', req, headers = headers)
                 except:
-                        handler.close()
+                        self.close()
+                        self.manager.feedback('err_write', self)
                         print_exc()
                         return None
                 try:
-                        ack  = handler.getresponse()
+                        ack  = self.handler.getresponse()
                         body = ack.read()
                 except:
-                        handler.close()
+                        self.close()
+                        self.manager.feedback('err_read', self)
                         print_exc()
                         return None
                 #--------------------------------------------------------
-                handler.close()
+                self.close()
+                self.manager.feedback('done', self)
 
                 key_val = {}
                 key_val['body']    = body
@@ -73,18 +89,16 @@ class ssl_worker(pp_thread):
 
 class ssl_login_worker(ssl_worker):
         def do_proc(self, arg):
-                global    server_dict
                 key_val, callback = arg
-
-                group     = key_val['group']
-                host_ip   = server_dict[group]['login']['ip']
-                host_name = server_dict[group]['login']['name']
 
                 proto     = proto_ssl_login(key_val)
                 req       = proto.make_login_req()
-                head      = proto.make_ssl_head(host_name)
-                info_val  = self.pyget(host_ip, req, head)
+                head      = proto.make_ssl_head(self.host_name)
+                info_val  = self.pyget(req, head)
                 #logger.debug(sorted(info_val.items()))
+
+                if info_val == None :
+                        return
 
                 if info_val['status'] != 200 :
                         logger.error('ack status error!!!')
@@ -99,23 +113,21 @@ class ssl_login_worker(ssl_worker):
                 #logger.debug(sorted(ack_val.items()))
 
                 if callback != None : callback(ack_val)
-                return ack_val
 
 class ssl_image_worker(ssl_worker):
         def do_proc(self, arg):
-                global    server_dict
                 key_val, callback = arg
 
                 price     = key_val['image_price']
-                group     = key_val['group']
-                host_ip   = server_dict[group]['toubiao']['ip']
-                host_name = server_dict[group]['toubiao']['name']
 
                 proto     = proto_ssl_image(key_val)
                 req       = proto.make_image_req(price)
-                head      = proto.make_ssl_head(host_name)
-                info_val  = self.pyget(host_ip, req, head)
+                head      = proto.make_ssl_head(self.host_name)
+                info_val  = self.pyget(req, head)
                 #logger.debug(sorted(info_val.items()))
+
+                if info_val == None :
+                        return
 
                 if info_val['status'] != 200 :
                         logger.error('ack status error!!!')
@@ -132,25 +144,23 @@ class ssl_image_worker(ssl_worker):
                 logger.debug(ack_sid)
 
                 if callback != None : callback(ack_val)
-                return ack_val
 
 class ssl_price_worker(ssl_worker):
         def do_proc(self, arg):
-                global    server_dict
                 key_val, callback = arg
 
                 price     = key_val['shot_price']
                 image     = key_val['image_decode']
                 sid       = key_val['sid']
-                group     = key_val['group']
-                host_ip   = server_dict[group]['toubiao']['ip']
-                host_name = server_dict[group]['toubiao']['name']
 
                 proto     = proto_ssl_price(key_val)
                 req       = proto.make_price_req(price, image)
-                head      = proto.make_ssl_head(host_name, sid)
-                info_val  = self.pyget(host_ip, req, head)
+                head      = proto.make_ssl_head(self.host_name, sid)
+                info_val  = self.pyget(req, head)
                 #logger.debug(sorted(info_val.items()))
+
+                if info_val == None :
+                        return
 
                 if info_val['status'] != 200 :
                         logger.error('ack status error!!!')
@@ -165,7 +175,6 @@ class ssl_price_worker(ssl_worker):
                 #logger.debug(sorted(ack_val.items()))
 
                 if callback != None : callback(ack_val)
-                return ack_val
 
 #==========================================================
 
@@ -173,26 +182,47 @@ class ssl_sender(pp_sender):
         def send(self, key_val, callback):
                 self.put((key_val, callback))
 
+        def feedback(self, status, worker):
+                logger.debug(worker.info + ' : ' + status)
+
 class ssl_login_sender(ssl_sender):
         def proc(self, arg):
-                worker = ssl_login_worker('ssl_login_worker')
+                global server_dict
+                group   = arg[0]['group']
+                key_val = {}
+                key_val['host_ip']      = server_dict[group]['login']['ip']
+                key_val['host_name']    = server_dict[group]['login']['name']
+                key_val['timeout']      = None
+                worker = ssl_login_worker(key_val, self, 'ssl_login_worker')
                 worker.start()
                 worker.wait_for_start()
-                worker.proc(arg)
+                worker.put(arg)
 
 class ssl_image_sender(ssl_sender):
         def proc(self, arg):
-                worker = ssl_image_worker('ssl_image_worker')
+                global server_dict
+                group   = arg[0]['group']
+                key_val = {}
+                key_val['host_ip']      = server_dict[group]['toubiao']['ip']
+                key_val['host_name']    = server_dict[group]['toubiao']['name']
+                key_val['timeout']      = None
+                worker = ssl_image_worker(key_val, self, 'ssl_image_worker')
                 worker.start()
                 worker.wait_for_start()
-                worker.proc(arg)
+                worker.put(arg)
 
 class ssl_price_sender(ssl_sender):
         def proc(self, arg):
-                worker = ssl_price_worker('ssl_price_worker')
+                global server_dict
+                group   = arg[0]['group']
+                key_val = {}
+                key_val['host_ip']      = server_dict[group]['toubiao']['ip']
+                key_val['host_name']    = server_dict[group]['toubiao']['name']
+                key_val['timeout']      = None
+                worker = ssl_price_worker(key_val, self, 'ssl_price_worker')
                 worker.start()
                 worker.wait_for_start()
-                worker.proc(arg)
+                worker.put(arg)
 
 #==========================================================
 
